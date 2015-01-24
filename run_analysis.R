4c9e14bf219353a68aa2b54d42c8054a4c27327d
benbrahim7777 <benbrahim777@yahoo.fr>
2015-01-24 16:35:37 -0500

    Updating Project
    Updating Project

--- run_analysis.R
+++ run_analysis.R
@@ -2,30 +2,83 @@
   
 #  https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
 
-#You should create one R script called run_analysis.R that does the following. 
-#Merges the training and the test sets to create one data set.
-#Extracts only the measurements on the mean and standard deviation for each measurement. 
-#Uses descriptive activity names to name the activities in the data set
-#Appropriately labels the data set with descriptive variable names. 
-#From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-
-#Good luck!
+# 1-create one R script called run_analysis.R that does the following. 
+# 2- Merges the training and the test sets to create one data set.
+# 3- Extracts only the measurements on the mean and standard deviation for each measurement. 
+# 4- Uses descriptive activity names to name the activities in the data set
+# 5- Appropriately labels the data set with descriptive variable names. 
+# 6- From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 
-Sys.setenv(JAVA_HOME='C:\\Program Files\\Java\\jre1.8.0_25')
+#getLibraries function
 
-setwd("C:/DropBox Salah/Coursera/getdata/Week3/")
+getLibrairies <- function() {
+  if (!require("data.table")) {
+    install.packages("data.table")
+  }
+  
+  if (!require("reshape2")) {
+    install.packages("reshape2")
+  }
+  
+  require("data.table")
+  require("reshape2")
+  
+}
 
-source ('./getLibrairies.R')
-getLibrairies()
 
-# Add code to get files from zipfile
+# function to get mean and deviation of test nd trin data
+
+
+get_mean_stdDeviation <- function(fileName)
+{
+  
+  my_features           <- read.table("./data/UCI HAR Dataset/features.txt")[,2]
+  my_mean_std_features  <- grepl("mean|std", my_features)
+  
+  
+  # get test data
+  my_x_test <- read.table(fileName)
+  
+  # extract mean and std data
+  names(my_x_test) = my_features      
+  my_x_test_mean_stdDeviation = my_x_test[,my_mean_std_features]
+  
+  #return extract data
+  return (my_x_test_mean_stdDeviation)
+}
+
+#function to get y_data
+
+get_y_data <- function(fileName)
+{
+  my_y_activity_labels  <- read.table("./data/UCI HAR Dataset/activity_labels.txt")[,2]
+  my_y_data             <- read.table(fileName)
+  
+  my_y_data[,2]     = my_y_activity_labels[my_y_data[,1]]
+  names(my_y_data)  = c("Activity_ID", "Activity_Label")
+  
+  return (my_y_data)
+  
+}
+
+#function to get subject data
+
+get_subject_data <- function(fileName)
+{
+  my_subject_test <- read.table(fileName)
+  
+  names(my_subject_test) = "subject"
+  
+  return (my_subject_test)
+  
+}
 
 
 # get column names for mean and std data of x test and x training
 # Extracts only the measurements on the mean and standard deviation for each measurement. 
 # Uses descriptive activity names to name the activities in the data set
-source('get_mean_stdDeviation.R')
+
 my_x_test_fileName <- "./data/UCI HAR Dataset/test/X_test.txt"
 my_x_test_mean_std <- get_mean_stdDeviation(my_x_test_fileName)
 
@@ -34,7 +87,7 @@ my_x_train_mean_std <- get_mean_stdDeviation(my_x_train_fileName)
 
 # get y test and y training labels
 # Uses descriptive activity names to name the activities in the data set
-source('get_y_test_data.R')
+
 my_y_test_fileName <- "./data/UCI HAR Dataset/test/y_test.txt"
 my_y_test_labels    <- get_y_data(my_y_test_fileName)
 
